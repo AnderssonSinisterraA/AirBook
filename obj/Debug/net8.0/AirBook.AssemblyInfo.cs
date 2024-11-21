2719935af6ac25328f8e8ebd4ba0d4a9eb713135
Andersson Sinisterra A <asinisterra2063@campuscecep.edu.co>
2024-11-21 14:45:51 -0500

    Razon Pages Creacion De Carpetas

--- obj/Debug/net8.0/AirBook.AssemblyInfo.cs
+++ obj/Debug/net8.0/AirBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AirBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c5caedc6853d19ae3df47eeb505a907533163b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0955d7f2d4eeb5303fd09ddc7f2f7536a9b4a9b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("AirBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AirBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
