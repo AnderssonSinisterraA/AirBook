cd30e0b787f44ffb8fedb766867118a765877114
Andersson Sinisterra A <asinisterra2063@campuscecep.edu.co>
2024-11-21 21:48:12 -0500

    Razon Pages Aerolinea Create

---
+++ Pages/Aerolinea/Create.cshtml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using AirBook.Data;
+using AirBook.Models;
+using System.Threading.Tasks;
+
+namespace AirBook.Pages.Aerolineas
+{
+    public class CreateModel : PageModel
+    {
+        private readonly AirBookContext _context;
+
+        public CreateModel(AirBookContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public AirBook.Models.Aerolinea Aerolinea { get; set; }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Aerolineas.Add(Aerolinea);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
--- obj/Debug/net8.0/AirBook.AssemblyInfo.cs
+++ obj/Debug/net8.0/AirBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AirBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0955d7f2d4eeb5303fd09ddc7f2f7536a9b4a9b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2719935af6ac25328f8e8ebd4ba0d4a9eb713135")]
 [assembly: System.Reflection.AssemblyProductAttribute("AirBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AirBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
