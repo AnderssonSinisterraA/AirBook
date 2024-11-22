0593ef9868bd0de02b15c0a7ca273a856719f9ef
Andersson Sinisterra A <asinisterra2063@campuscecep.edu.co>
2024-11-21 21:49:09 -0500

    Razon Pages Aerolinea Index

---
+++ Pages/Aerolinea/Index.cshtml.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AirBook.Data;
+using AirBook.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AirBook.Pages.Aerolineas
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AirBookContext _context;
+
+        public IndexModel(AirBookContext context)
+        {
+            _context = context;
+        }
+
+        public IList<AirBook.Models.Aerolinea> Aerolineas { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Aerolineas = await _context.Aerolineas.ToListAsync();
+        }
+    }
+}
--- obj/Debug/net8.0/AirBook.AssemblyInfo.cs
+++ obj/Debug/net8.0/AirBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AirBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2719935af6ac25328f8e8ebd4ba0d4a9eb713135")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd30e0b787f44ffb8fedb766867118a765877114")]
 [assembly: System.Reflection.AssemblyProductAttribute("AirBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AirBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
