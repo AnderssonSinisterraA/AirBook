d348361052c3f275e7e2c80806ec29abfb149d50
Andersson Sinisterra A <asinisterra2063@campuscecep.edu.co>
2024-11-23 10:45:09 -0500

    Razon Pages Itenerario Details

--- obj/Debug/net8.0/AirBook.AssemblyInfo.cs
+++ obj/Debug/net8.0/AirBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AirBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dba1e5372ac2a562cab9c705249c7161b318582")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4abdf8e39e2bd4dae35a74e4b30d6645c327a89")]
 [assembly: System.Reflection.AssemblyProductAttribute("AirBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AirBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
