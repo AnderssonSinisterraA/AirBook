0daae05bd92deacbffc36b3f49ae29b40024e004
Andersson Sinisterra A <asinisterra2063@campuscecep.edu.co>
2024-11-23 10:54:40 -0500

    Razon Pages Reserva Create

---
+++ Pages/Reserva/Create.cshtml.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using AirBook.Data;
+using AirBook.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirBook.Pages.Reservas
+{
+    public class CreateModel : PageModel
+    {
+        private readonly AirBookContext _context;
+
+        public CreateModel(AirBookContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public AirBook.Models.Reserva Reserva { get; set; }
+        public SelectList Pasajeros { get; set; }
+        public SelectList Vuelos { get; set; }
+
+        public IActionResult OnGet()
+        {
+            Pasajeros = new SelectList(_context.Pasajeros, "IdPasajero", "Nombre");
+            Vuelos = new SelectList(_context.Vuelos, "IdVuelo", "NumeroVuelo");
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                Pasajeros = new SelectList(_context.Pasajeros, "IdPasajero", "Nombre");
+                Vuelos = new SelectList(_context.Vuelos, "IdVuelo", "NumeroVuelo");
+                return Page();
+            }
+
+            // Verificar que IdPasajero y IdVuelo no sean NULL
+            if (Reserva.IdPasajero == 0 || Reserva.IdVuelo == 0)
+            {
+                ModelState.AddModelError(string.Empty, "Debe seleccionar un pasajero y un vuelo.");
+                Pasajeros = new SelectList(_context.Pasajeros, "IdPasajero", "Nombre");
+                Vuelos = new SelectList(_context.Vuelos, "IdVuelo", "NumeroVuelo");
+                return Page();
+            }
+
+            _context.Reservas.Add(Reserva);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
--- obj/Debug/net8.0/AirBook.AssemblyInfo.cs
+++ obj/Debug/net8.0/AirBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AirBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27bb8a62b51e473ee253dcdc10b052d62e2e3789")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74e3f58b34f79851b985bbdfe1d721d695394305")]
 [assembly: System.Reflection.AssemblyProductAttribute("AirBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AirBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
